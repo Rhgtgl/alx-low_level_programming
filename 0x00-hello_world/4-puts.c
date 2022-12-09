5f8d89e285a14b51890c5173413d520143154ee4
rhgtgl <georgesylvio@gmail.com>
2022-12-08 18:08:52 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 
 
 /** 
- * main - Entry point
- *
+ *main - Entry point
+ 
  *Return: Always 0 (success)
  */
 
 int main(void)
 {
-	puts(*\'Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
