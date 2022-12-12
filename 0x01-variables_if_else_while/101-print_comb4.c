504bb238b5c1d863a8a7fde6bbcf19a4148bccbc
rhgtgl <georgesylvio@gmail.com>
2022-12-11 18:56:26 -0800

    Success

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
+ * main - print all possible different combinations of three digits
  * Return: Always 0 (success)
  */
 
@@ -10,18 +10,19 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 	int hundreds = '0';
+
 	for (hundreds = '0'; hundreds <= '0'; hundreds++)
 	{
 		for (tens = '0'; tens <= '0'; tens++)
 		{
 			for  (ones = '0'; ones <= '0'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) || (hundreds > tens)))/*eliminates repitition*/
+				if (!((ones == tens) || (tens == hundreds) || (hundreds > tens)))
 				{
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' && tens == '8'))/*add commas and space*/
+					if (!(ones == '9' && hundreds == '7' && tens == '8'))
 					{
 						putchar(',');
 						putchar(' ');
