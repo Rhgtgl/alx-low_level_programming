bbb19cc3fa59fe2373357135b744dc598ab2277a
rhgtgl <georgesylvio@gmail.com>
2022-12-12 01:25:44 -0800

    3ps of success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+ * main - print all possible combinations of single digits
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int g;
+	
+
+	for (g = 48; g < 58; g++)
+	{
+		putchar(g);
+		if (g != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
