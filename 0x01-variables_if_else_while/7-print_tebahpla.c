62c312312ea9879a071f6800403f63281cb197fb
rhgtgl <georgesylvio@gmail.com>
2022-12-12 00:52:28 -0800

    Mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - print lowercase alphabets in reverse a-z
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char rhg;
+
+	for (rhg = 'z'; rhg >= 'a'; rhg++)
+		putchar(rhg);
+
+	putchar('\n');
+	return (0);
+}
