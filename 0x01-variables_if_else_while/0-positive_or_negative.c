59a6c94895e4e20266d74c5dca53ff11da3ce7fe
rhgtgl <georgesylvio@gmail.com>
2022-12-11 18:10:03 -0800

    Stay positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,12 +12,13 @@
 int main(void)
 {
 	int n;
-	srand(time(0));
+	srand(time(0)); 
+
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
-		printf("%d is positve\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
