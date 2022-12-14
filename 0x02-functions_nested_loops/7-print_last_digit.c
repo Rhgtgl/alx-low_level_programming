75cbd94771f6d9d765ebc8da26e067be923e99b0
rhgtgl <georgesylvio@gmail.com>
2022-12-13 18:19:29 -0800

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * print_last_digit - prints last digit of a numbers
+ * @n: number
+ * Return: the value of the last digit
+ */
+
+
+int print_last_digit(int n)
+{
+	int tgl = n % 10;
+
+	if (tgl < 0)
+		tgl *= -1;
+	_putchar(tgl + '0');
+
+
+	return (0);
+
