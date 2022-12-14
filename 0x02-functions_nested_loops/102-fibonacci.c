2016e5eac7b8f668c47699293228a60b4da4bb7b
rhgtgl <georgesylvio@gmail.com>
2022-12-14 00:01:23 -0800

    First fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main _ any
+ * Return: any
+ */
+
+int main(void)
+
+{
+	long int t, a, l, b;
+
+	a = 1;
+	l = 2;
+
+	for (t = 1; t <= 50; ++t)
+	{
+		if (a != 20365011074)
+		{
+			printf("%bd,", a);
+		}
+
+		else
+		{
+			printf("%bd\n", a);
+		}
+
+		b = a + l;
+		a = l;
+		l = b;
+	}
+
+	return (0);
+}
