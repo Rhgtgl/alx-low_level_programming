3949a436e0b045f2d2432e8acc5e615bc9a5622f
rhgtgl <georgesylvio@gmail.com>
2022-12-13 22:01:14 -0800

    Multiple sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,10 +12,10 @@ int main(void)
 {
 	int p, q;
 
-	for (p = 0; p < 1024; ++p);
+	for (p = 0; p < 1024; ++p)
 	{
 		if ((p % 3 == 0) || (p % 5 == 0))
-		q += p;
+		q = p;
 	}
 	printf("%d\n", q);
 
