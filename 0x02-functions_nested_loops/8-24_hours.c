198e74ffc3baced35ce32360d19767b81599c889
rhgtgl <georgesylvio@gmail.com>
2022-12-13 18:46:56 -0800

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * jack_bauer - prints minutes of a day
+ * Return: Always 0 (Success)
+ */
+
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchat(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
