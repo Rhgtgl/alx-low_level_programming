5431ca28396802e10b95e5b2ddbd5fd347f74913
rhgtgl <georgesylvio@gmail.com>
2022-12-13 16:56:19 -0800

    Lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ */
+
+
+void print_alphabet(void)
+{
+	char rhg;
+
+	
+	for (rhg = 'a'; rhg <= 'z'; rhg++)
+		_putchar(rhg);
+
+
+	_putchar('\n');
+}
