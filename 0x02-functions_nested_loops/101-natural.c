f0bbb602e96d978bcee6b18e01cab72817c380f0
rhgtgl <georgesylvio@gmail.com>
2022-12-13 21:34:44 -0800

    Multiple sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * compute sum of multiples of 3 and 5 below 1024
+ * Return: Always 0 
+ */
+
+int main(void)
+
+{
+	int p, q;
+
+	for (p = 0, p < 1024; ++p)
+	{
+		if ((p % 3 == 0) || (p % 5 == 0))
+		q += p;
+	}
+	printf("%d\n", q);
+
+	return (0);
+}
