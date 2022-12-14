2fb7e075606b6118271e1a08688ea25d06b76ec1
rhgtgl <georgesylvio@gmail.com>
2022-12-13 20:44:37 -0800

    100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - print the 100th multiplication the table
+ * @p: value
+ * Return: Always 0 
+ */
+
+void print_times_table(int q)
+{
+	int t;
+	int g;
+	int l;
+
+	if (p < 0 || p >= 15)
+		return;
+	for (t =0; t <= p; t++)
+	{
+		for (g = 0; g <= p; g++)
+		{
+			l = (t * g);
+			if (g == 0)
+				_putchar('0' + l);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (l <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + l);
+				}
+				else if (l > 9 && l < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (l / 10));
+					_putchar('0' + (l % 10));
+				}
+				else if (l >=100)
+			}
+					_purchar('0' + (l / 100));
+					_putchar('0' + ((l / 10) % 10));
+					_putchar('0' + (l % 10));
+		}
+	}
+	_putchar('\n');
+}
