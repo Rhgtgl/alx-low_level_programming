87bd7066ffd210f1dd3814da6add5d3f570aeb65
rhgtgl <georgesylvio@gmail.com>
2022-12-19 03:30:59 -0800

    Numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,12 +8,13 @@
  */
 int _atoi(char *s)
 {
-	unsigned int count = 0, j = 0, size = 0, m = 1, i;
+	unsigned int count = 0, j = 0, size = 0, m = 1, k = 1, i;
 
 	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
+
 		if (*(s + count) == '-')
 			k *= -1;
 
