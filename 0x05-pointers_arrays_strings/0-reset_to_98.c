b233034aca45738084f9dd9642ec6b981d24f0f5
rhgtgl <georgesylvio@gmail.com>
2022-12-19 14:45:45 -0800

    Transmitter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - update pointer value to 98
+ * @n: input parameter
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
