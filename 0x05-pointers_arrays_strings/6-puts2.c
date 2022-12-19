2cb207d887b43156b725eaeceb91ca3c57b136d5
rhgtgl <georgesylvio@gmail.com>
2022-12-19 12:55:33 -0800

    Half not true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - function to prints strings every character of a string
+ * @str: input pointer value
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
