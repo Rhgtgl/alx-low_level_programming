bb67314dd6044b49a341671095babbe902e34ca7
rhgtgl <georgesylvio@gmail.com>
2022-12-21 18:48:56 -0800

    Change

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string-toupper - function change all to uppercase
+ * @word: pointer to string
+ * Return: Pointer to uppercase
+ */
+
+char *string_toupper(char *word)
+{
+	int length;
+
+	length = 0;
+
+	while (word[length] != '\0')
+	{
+		if (word[length] >= 97 && word[length] = word[length] <= 122)
+		{
+			word[length] = word[length] - 32;
+		}
+		length++;
+	}
+	return (word);
+}
+
