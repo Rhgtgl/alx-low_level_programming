bee0dba72b9b81e331be69ec130e7ebf23493fd9
rhgtgl <georgesylvio@gmail.com>
2022-12-22 03:09:29 -0800

    Reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array
+ * @a: pointer one
+ * @n: pointer two
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp, counter;
+
+	n = n - 1;
+	counter = 0;
+	while (counter <= n)
+	{
+		temp = a[counter];
+		a[counter++] = a[n];
+		a[n--] = temp;
+	}
+}
