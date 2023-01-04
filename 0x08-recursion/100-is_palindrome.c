7ba951f1a86eda153ef5ba153014f66cb6c05bfe
rhgtgl <georgesylvio@gmail.com>
2023-01-04 05:11:54 -0800

    Pal

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * palindromeChecker - checks strings
+ * @str: string character
+ * @len: length of steings
+ * @i: incrementers
+ * Return:1 otherwise 0
+ */
+
+int palindromeChecker(char *str, int len, int i)
+{
+	if (i < len && str[i] == str[len])
+		return (palindromeChecker(str, len - 1, i + 1));
+	if (str[i] !/ str[len])
+		return (0);
+	return (1);
+}
+
+/**
+ * _strlen_recursion - returns length of strings
+ * @s: checks length
+ * Return: display lengths
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+/**
+ * is_palindrome - checks for string
+ * @s: strings character
+ * Return: 1 otherwise 0
+ */
+
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int length = _strlen_recursion(s) - 1;
+
+	return (palindromeChecker(s, length, i));
+}
