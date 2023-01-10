83f0fa7f3f6d0735149ad65a89531d6e52637cf4
rhgtgl <georgesylvio@gmail.com>
2023-01-10 15:03:23 -0800

    Greatness

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,7 +7,7 @@
  * Return: number of words
  */
 
-int count_word(chat *s)
+int count_word(char *s)
 {
 	int flag, c, w;
 
@@ -44,7 +44,7 @@ char **strtow(char *str)
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
-	matrix = (char **) malloc(sizeof(chat *) * (words + 1));
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
 	for (i = 0; i <= len; i++)
