f92b0118273f5cffbb8d85e59594b55de4b94297
rhgtgl <georgesylvio@gmail.com>
2023-01-11 16:44:53 -0800

    Float and sting

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an arrays of chars
+ * @size: size of the arrays
+ * @c: strings
+ * Return: 0 successful
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	buffer = (char *) malloc(size * sizeof(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+		while (position < size)
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+		return (buffer);
+	}
+}
