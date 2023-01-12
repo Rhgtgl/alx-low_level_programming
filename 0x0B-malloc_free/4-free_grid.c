4943ec748f6add6784924bcda1dd842a62009a50
rhgtgl <georgesylvio@gmail.com>
2023-01-11 16:27:00 -0800

    Freedom

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - function allocates a grid
+ * @grid: width of grid
+ * @height: height of grid
+ * Return: free grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
