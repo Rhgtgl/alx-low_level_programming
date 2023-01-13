85c907be43fb963492285eecd10a90c0005794d7
rhgtgl <georgesylvio@gmail.com>
2023-01-12 18:54:21 -0800

    Memory space allocator

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#inclde: "main.h"
+
+/**
+ * malloc_checked - function allocates memory
+ * @b: integer variable
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
