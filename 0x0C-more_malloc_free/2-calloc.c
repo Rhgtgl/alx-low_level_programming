da9e3eb2cceb4292191a159f67302d1438272ae4
rhgtgl <georgesylvio@gmail.com>
2023-01-12 18:05:30 -0800

    Calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * _calloc - function allocates memory
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	P = malloc(nmemb * size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	_memset(p, 0, (nmemb * size));
+
+	return (p);
+}
