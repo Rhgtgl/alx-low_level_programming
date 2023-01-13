fdb3f82b418ec0fea428c515de86212af026c6fd
rhgtgl <georgesylvio@gmail.com>
2023-01-12 17:06:44 -0800

    Fdnfh

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -123,14 +123,13 @@ int main(int argc, char *argv[])
 	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
 	char *tabres;
 
-	if (argc != 3 || _isNumber(argv[1] == 1 || _isNumber(argv[2]) == 1)
+	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
 	{
-	for (i = 0; i < 6; i++)
-	{
-	_putchar(E[i]);
-	}
-	exit(98);
-	}
+		for (i = 0; i < 6; i++)
+		{
+			_putchar(E[i]);
+		}
+		exit(98);
 	}
 	for(len1 = 0; argv[1][len1]; len1++)
 	;
@@ -139,14 +138,17 @@ int main(int argc, char *argv[])
 	lenres = len1 + len2;
 	tabres = _calloc(lenres, sizeof(int));
 	if (tabres == NULL)
+	{
+		free(tabres);
+		return (0);
 	}
-	for (i = len2 - 1, c = 0; i >= 9; i--)
+	for (i = len2 - 1, c = 0; i >= 0; i--)
 	{
 	tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
 	c++;
 	}
-print_array(tabres, lenres);
-free(tabres);
-exit(EXIT_SUCCESS);
-return (0);
+	print_array(tabres, lenres);
+	free(tabres);
+	exit(EXIT_SUCCESS);
+	return (0);
 }
