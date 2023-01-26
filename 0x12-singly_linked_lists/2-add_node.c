67b16c78283b5368401c0354f708d52c2442647c
rhgtgl <georgesylvio@gmail.com>
2023-01-26 13:35:50 -0800

    Addition

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: head of the linked list
- * @str: string to store in the list
- * Return: address of the head
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -19,10 +20,10 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[a])
 		a++;
 
-		new->str = strdup(str);
-		new->len = a;
-		new->next = *head;
-		*head = new;
+	new->str = strdup(str);
+	new->len = a;
+	new->next = *head;
+	*head = new;
 
 	return (*head);
 }
