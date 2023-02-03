e10014aec6a6ee6563b26667d508c460d68dc8d8
rhgtgl <georgesylvio@gmail.com>
2023-02-02 18:20:10 -0800

    Ge

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - function checks foe endianness
+ * Return: 0 otherwise 1
+ */
+
+int get_endianness(void)
+{
+	unsigned int b = 1;
+	char *a = (char *)&b;
+
+	if (*a)
+		return (1);
+	else
+	return (0);
+}
