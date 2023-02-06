5f89b73d6bbf77b2e087f37191707a0835edcfb5
rhgtgl <georgesylvio@gmail.com>
2023-02-06 15:29:31 -0800

    Cf

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and adds permission
+ * @filename: The name
+ * @text_content: content
+ * Return: 1 otherwise -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fild = 0, chk = 0, i = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	fild = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	if (fild < 0)
+		return (-1);
+
+	while (text_content[i])
+		i++;
+	chk = write(fild, text_content, i);
+	if (chk < 0)
+		return (-1);
+
+	close(fild);
+	return (1);
+}
