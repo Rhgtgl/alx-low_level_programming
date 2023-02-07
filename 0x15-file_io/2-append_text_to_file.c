931ec41f6b3146e9336c5d65de191cc2d9a6e96a
rhgtgl <georgesylvio@gmail.com>
2023-02-06 17:12:09 -0800

    Atf

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,10 +23,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[i])
 			i++;
 
-			chk = write(fild, text_content, i);
+		chk = write(fild, text_content, i);
 		if (chk < 0)
-			return (-1);
+		return (-1);
 	}
-		close(fild);
-		return (1);
+	close(fild);
+	return (1);
 }
